8c44cde29eb215e82730cb256b0c52dbc2919929
Fedorov <chugun777akk@mail.ru>
2023-05-15 08:53:03 +0700

    all tests passed

--- Source.cpp
+++ Source.cpp
@@ -14,8 +14,30 @@ struct ListNode {
 
 class Solution {
 public:
-	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+	{
+		ListNode* current = new ListNode();                 
+		ListNode* head = current;                            
+		while (list1 != NULL && list2 != NULL)               
+		{
+			if (list1->val <= list2->val)                   
+			{
+				head->next = list1;                        
+				list1 = list1->next;                     
+			}
+			else
+			{
+				head->next = list2;                            
+				list2 = list2->next;                            
+			}
+			head = head->next;                                  
+		}
+		if (list1 != NULL)
+			head->next = list1;
+		else
+			head->next = list2;
 
+		return current->next;
 	}
 };
 
@@ -24,8 +46,5 @@ public:
 
 int main()
 {
-	ListNode list1(1,3,4 );
-	ListNode list2;
-
-	return 0;
+	
 }
